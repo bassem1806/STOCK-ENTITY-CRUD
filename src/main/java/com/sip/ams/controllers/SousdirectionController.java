90fee43ec15049cd44276029d0dd31d7fb992666
bassem1806 <bendhief.b@gmail.com>
2022-04-20 12:24:43 +0100

    ajout sous direction

--- src/main/java/com/sip/ams/controllers/SousdirectionController.java
+++ src/main/java/com/sip/ams/controllers/SousdirectionController.java
@@ -1,9 +1,10 @@
 package com.sip.ams.controllers;
 
-import java.util.List;
-
-import javax.validation.Valid;
-
+import com.sip.ams.entities.Direction;
+import com.sip.ams.entities.Sousdirection;
+import com.sip.ams.repositories.DirectionRepository;
+import com.sip.ams.repositories.DirectiongRepository;
+import com.sip.ams.repositories.SousdirectionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,78 +14,70 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.sip.ams.entities.Direction;
-import com.sip.ams.entities.Sousdirection;
-import com.sip.ams.repositories.DirectionRepository;
-import com.sip.ams.repositories.DirectiongRepository;
-import com.sip.ams.repositories.SousdirectionRepository;
+import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequestMapping("/sousdirection/")
 public class SousdirectionController {
-	
-	
-	private final SousdirectionRepository sousdirectionRepository;
-	private final DirectionRepository directionRepository;
-	private final DirectiongRepository directiongRepository;
-	
-	@Autowired
-	 public SousdirectionController(SousdirectionRepository sousdirectionRepository,DirectionRepository directionRepository, DirectiongRepository directiongRepository) {
-       
-		this.sousdirectionRepository = sousdirectionRepository;
-		this.directionRepository = directionRepository ;
-       this.directiongRepository = directiongRepository;
-   }
-	
-	@GetMapping("list")
+
+
+    private final SousdirectionRepository sousdirectionRepository;
+    private final DirectionRepository directionRepository;
+    private final DirectiongRepository directiongRepository;
+
+    @Autowired
+    public SousdirectionController(SousdirectionRepository sousdirectionRepository,DirectionRepository directionRepository, DirectiongRepository directiongRepository) {
+
+        this.sousdirectionRepository = sousdirectionRepository;
+        this.directionRepository = directionRepository ;
+        this.directiongRepository = directiongRepository;
+    }
+
+    @GetMapping("list")
     //@ResponseBody
     public String listSousdirections(Model model) {
-    	
-    	List<Sousdirection> lp = (List<Sousdirection>)sousdirectionRepository.findAll();
-    	
-    	if(lp.size()==0)
-    		lp = null;
+
+        List<Sousdirection> lp = (List<Sousdirection>)sousdirectionRepository.findAll();
+
+        if(lp.size()==0)
+            lp = null;
         model.addAttribute("sousdirections", lp);
-        
+
         return "sousdirection/listSousdirections";
 
     }
-	
-	 @GetMapping("add")
-	    public String showAddSousdirectionForm(Sousdirection sousdirection, Model model) {
-		
-		
-		        model.addAttribute("directiong", directionRepository.findAll());
-	  	    	model.addAttribute("directiongs", directiongRepository.findAll());
-	  	    	model.addAttribute("sousdirection", new Sousdirection());
-	          
-	        return "sousdirection/addSousdirection";
-	    }
-	 
-	 
-	 @PostMapping("addSave")
-		
-	 public String addSousdirection(@Valid Sousdirection sousdirection, BindingResult result, 
-			
-			 @RequestParam(name = "directionId", required = true) Long dr)
-	      
-	    		
-	    		 {
 
-	    
-			
-			
-			Direction direction = directionRepository.findById(dr).orElseThrow(()-> new IllegalArgumentException
-				 ("Invalid direction Id:" +dr));
-		
-	    	sousdirection.setDirection(direction);
-	    	
-	    	
-	    			   		    		   		    	
-	   	sousdirectionRepository.save(sousdirection);
- 	 return "redirect:list";
-	
-	    	 }
-	 
+    @GetMapping("add")
+    public String showAddSousdirectionForm(Sousdirection sousdirection, Model model) {
+
+
+
+        model.addAttribute("directiongs", directiongRepository.findAll());
+        model.addAttribute("directions", directionRepository.findAll());
+        model.addAttribute("sousdirection", new Sousdirection());
+
+        return "sousdirection/addSousdirection";
+    }
+
+
+    @PostMapping("addSave")
+
+    public String addSousdirection(@Valid Sousdirection sousdirection, BindingResult result,
+
+                                   @RequestParam(name = "directionId", required = true) Long d)
+
+    {
+
+        Direction direction= directionRepository.findById(d).orElseThrow(()-> new IllegalArgumentException
+                ("Invalid direction Id:" +d));
+        sousdirection.setDirection(direction);
+
+
+        sousdirectionRepository.save(sousdirection);
+        return "redirect:list";
+
+    }
+
 
 }
